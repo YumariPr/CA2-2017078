a261ac3e4e028b1071cecc31846d5a87e71fe0ad
yumarip <pryumarij@gmail.com>
2020-04-25 20:06:28 +0000

    already save data in MongoDB but I can not get my web page

--- api/models/user.js
+++ api/models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require( 'mongoose');
 
 var userSchema = new mongoose. Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+    //_id: mongoose.Schema.Types.ObjectId,
     item: String,
     price: Number
 });
